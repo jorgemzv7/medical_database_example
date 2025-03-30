7578907c39733f8ceca6d5a013d40953682468b3
Ventura <jorge@softpoint.es>
2025-03-30 15:26:48 +0200

    :wrench: Updated db_url secret

--- app/config.py
+++ app/config.py
@@ -1,13 +1,5 @@
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import BaseSettings
+import os
 
 class Settings(BaseSettings):
-    db_url: PostgresDsn  # Valida formato PostgreSQL
-    app_env: str = 'production'
-    debug: bool = False
-    secret_key: str
-
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-
-settings = Settings()
+    db_url: str = os.getenv('DB_URL') or open('/run/secrets/db_url').read()
