c02b8fb2cba0af75666f1e5a0f915f215c0c57b3
Ventura <jorge@softpoint.es>
2025-03-30 18:04:47 +0200

    :wrench: Init changes in API and DB

--- app/database.py
+++ app/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 # Load environment variables
 DB_URL = os.getenv('DB_URL')
 
-engine = create_async_engine(DB_URL)
+engine: AsyncEngine = create_async_engine(DB_URL)
 async_session = sessionmaker(
     engine, 
     expire_on_commit=False, 
--- app/main.py
+++ app/main.py
@@ -2,24 +2,27 @@ from fastapi import FastAPI
 from app.routes import patient_leads
 from app.database import engine, Base
 from dotenv import load_dotenv
-from app.models.person import Person
-from app.models.hospital import Hospital
-from app.models.physician import Physician
-from app.models.contact_booking import ContactBooking
-from app.models.eap_dossier import EAPDossier
-from app.models.medical_condition import MedicalCondition
-from app.models.patient_lead import PatientLead
-from app.models.referal import Referal
-
-
-Base.metadata.create_all(bind=engine)
 
 load_dotenv()
+
+async def lifespan(app: FastAPI):
+    # Startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+    yield
+
+    # Shutdown
+    await engine.dispose()
+
+
 app = FastAPI(
     title='myTomorrows CRM API',
     description='API for patient and contact management',
-    version='1.0.0'
+    version='1.0.0',
+    lifespan=lifespan
 )
+
 
 app.include_router(patient_leads.router)
 
