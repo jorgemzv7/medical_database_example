7b6c2ed0cf93558fb70dbceb08c978323b9a62b0
Ventura <jorge@softpoint.es>
2025-03-30 21:03:15 +0200

    :wrench: Added indexes and default ids

--- app/models/patient_lead.py
+++ app/models/patient_lead.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, ForeignKey, DateTime, Boolean
+import uuid
+from sqlalchemy import Column, ForeignKey, DateTime, Boolean, Index
 from sqlalchemy.dialects.postgresql import UUID
 from app.database import Base
 
@@ -6,7 +7,7 @@ from app.database import Base
 class PatientLead(Base):
     __tablename__ = 'patient_leads'
     
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     person_id = Column(UUID(as_uuid=True), ForeignKey('persons.id'), nullable=False)
     medical_condition_id = Column(UUID(as_uuid=True), ForeignKey('medical_conditions.id'))
     physician_id = Column(UUID(as_uuid=True), ForeignKey('physicians.id'))
@@ -14,3 +15,12 @@ class PatientLead(Base):
     valid_from = Column(DateTime, nullable=False) # Initial consult record now
     valid_to = Column(DateTime, nullable=True)
     is_current = Column(Boolean, default=True)  # current record
+
+    __table_args__ = (
+        Index(
+            'uq_person_is_current',
+            'person_id',
+            unique=True,
+            postgresql_where=(is_current.is_(True))
+        ),
+    )
--- app/models/physician.py
+++ app/models/physician.py
@@ -1,13 +1,12 @@
+from uuid import uuid4
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from app.database import Base
 
-
-
 class Physician(Base):
     __tablename__ = 'physicians'
     
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     person_id = Column(UUID(as_uuid=True), ForeignKey('persons.id'), nullable=False)
     hospital_id = Column(UUID(as_uuid=True), ForeignKey('hospitals.id'), nullable=True) # ? Add physicians without hospital
     medical_licence_number = Column(String(32), unique=True, nullable=False)
