f5d9a7f830f5313cf220aabb8d6ea609b3ebe5e7
Ventura <jorge@softpoint.es>
2025-03-30 18:04:56 +0200

    :wrench: Deleted schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,9 +1,5 @@
-from .person import PersonBase, PersonCreate, PersonUpdate, PersonResponse
-from .physician import PhysicianBase, PhysicianCreate, PhysicianUpdate, PhysicianResponse
 from .patient_lead import PatientLeadBase, PatientLeadCreate, PatientLeadUpdate, PatientLeadResponse
 
 __all__ = [
-    'PersonBase', 'PersonCreate', 'PersonUpdate', 'PersonResponse',
-    'PhysicianBase', 'PhysicianCreate', 'PhysicianUpdate', 'PhysicianResponse',
     'PatientLeadBase', 'PatientLeadCreate', 'PatientLeadUpdate', 'PatientLeadResponse'
 ]
